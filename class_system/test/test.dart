e9e07c61922cb87b7e870290e2c78c96c6af64f0
Hu <aa254412@gmail.com>
2024-08-31 16:12:04 +0800

    feats: 新增 test.dart

---
+++ class_system/test/test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:class_system/main.dart';
+import 'package:class_system/services/courseRepository.dart';
+
+void main() {
+  // test('測試', () => null)
+}
